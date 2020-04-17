340f452425f6d506d64bbfe93403667309a95b97
David Scott <david.r.scott@postgrad.curtin.edu.au>
2020-04-17 14:13:31 +1000

    Bugfixes and launch script for sum.py

---
+++ output/launch_sum.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRB=$1
+pol=$2
+out=$3
+
+module load gcc/6.4.0 
+module load python/3.7.4
+module load numpy/1.18.2-python-3.7.4
+
+python sum.py -p $pol -o $out  $FRB
--- output/sum.py
+++ output/sum.py
@@ -3,7 +3,7 @@ Sums time series together for a given FRB
 """
 
 import numpy as np
-from argpase import ArgumentParser, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import glob
 
 def _main():
@@ -26,28 +26,28 @@ def _main():
 	else:
 		print("Invalid polarisation")
 
-def get_args()
+def get_args():
 	parser = ArgumentParser(description="Sums time series together for a given FRB", formatter_class=ArgumentDefaultsHelpFormatter)
 
 	parser.add_argument(dest='FRB', type=str, help='FRB to sum')
-	parser.add_argument('-c', type='store_true', help='Use cropped time series instead of full', default=False)
+	#parser.add_argument('-c', type='store_true', help='Use cropped time series instead of full', default=False)
 	# TODO: Implement -c flag
 	parser.add_argument('-o', type=str, help='Output filename (polarisation will be appended as prefix)', default='sum_t.npy')
 	parser.add_argument('-p', type=str, help='Polarisation to do (default is both)', default='both')
 
 	return parser.parse_args()
 
-def find_files(FRB):
+def find_files(FRB, p):
 	glob_str_x = '{}/*x*t.npy'.format(FRB)
 	glob_str_y = '{}/*y*t.npy'.format(FRB)
 
 	x_fnames = glob.glob(glob_str_x)
 	y_fnames = glob.glob(glob_str_y)
 
-	if args.p == 'both':
+	if p == 'both':
 		# Return lists for x and y
 		return x_fnames, y_fnames
-	elif args.p == 'x':
+	elif p == 'x':
 		return x_fnames, None
 	else:	# must be 'y'
 		return None, y_fnames
@@ -56,19 +56,24 @@ def do_sum(fnames):
 	# Sum one file at a time, since they're very large
 	# Numpy's mmap_mode helps by allowing us to do this without moving them into memory
 
+	print(fnames)
+
 	# Initialise summation array with the first file
+	print("Initialising sum array...")
 	sum_arr = np.load(fnames[0]).copy()
 
 	# Go over the rest of the files, summing their contents into sum_arr
 	for fname in fnames[1:]:
+		print(fname)
 		# mmap_mode='r' keeps the loaded array on disk, instead of loading it into memory
 		sum_arr += np.load(fname, mmap_mode='r')
 	
 	return sum_arr
 
 def save(arr, outfile, prefix):
+	print("Saving...")
 	dest_fname = prefix + outfile
-	np.save(darr,dest_fname)
+	np.save(dest_fname, arr)
 
 if __name__ == "__main__":
 	_main()
