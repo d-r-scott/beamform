0a4d9f74a6e8319acbdca18ec7fe356e904d6a58
David Scott <david.r.scott@postgrad.curtin.edu.au>
2020-06-09 12:46:04 +1000

    Fixed python3 modules needing gcc to work

--- modules.sh
+++ modules.sh
@@ -3,8 +3,8 @@
 # Script to export sets of modules required
 
 modules_1="python/2.7.14 numpy/1.16.3-python-2.7.14 scipy/1.0.0-python-2.7.14 astropy/2.0.3-python-2.7.14 matplotlib/2.2.2-python-2.7.14"
-modules_2="python/3.7.4 numpy/1.18.2-python-3.7.4"
+modules_2="gcc/7.3.0 openmpi/3.0.0 python/3.7.4 numpy/1.18.2-python-3.7.4"
 modules_3="python/2.7.14 numpy/1.16.3-python-2.7.14 scipy/1.0.0-python-2.7.14"
-modules_4="python/3.7.4 numpy/1.18.2-python-3.7.4"
-modules_5="python/3.7.4 numpy/1.18.2-python-3.7.4 scipy/1.4.1-python-3.7.4"
-modules_6="python/3.7.4 numpy/1.18.2-python-3.7.4"
+modules_4="gcc/7.3.0 openmpi/3.0.0 python/3.7.4 numpy/1.18.2-python-3.7.4"
+modules_5="gcc/7.3.0 openmpi/3.0.0 python/3.7.4 numpy/1.18.2-python-3.7.4 scipy/1.4.1-python-3.7.4"
+modules_6="gcc/7.3.0 openmpi/3.0.0 python/3.7.4 numpy/1.18.2-python-3.7.4"
