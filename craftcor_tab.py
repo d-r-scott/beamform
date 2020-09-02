84850694aec1790940b0d771dbd6114966769f36
d_r_scott <david.r.scott@postgrad.curtin.edu.au>
2020-09-02 12:04:48 +0800

    change exit to exit()

--- craftcor_tab.py
+++ craftcor_tab.py
@@ -254,7 +254,7 @@ class AntennaSource(object):
         rawd_fname = 'output/200430/f/raw_{}.npy'.format(self.antno)
         np.save(rawd_fname, rawd)
 
-        exit
+        exit()
 
         data_out = np.zeros((corr.nint, corr.nfine_chan, corr.npol_in), dtype=np.complex64)
         d1 = data_out
